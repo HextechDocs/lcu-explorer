7457bd92dfb8479ec6524e5792fc21c45542f247
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-10-09 16:45:26 -0700

    Handle closing/opening of LCU
    Makes LCU Explorer more robust in the face of opening/closing/reopening
    of the LCU client, and slightly faster. FE keeps all the state in
    `useState` calls which allow the Swagger UI, OpenAPI spec JSON, and the
    LCU credentials to load (or unload/reload) in any order. A basic plugin
    updates the Swagger UI on changes.
    
    When not connected to the LCU the endpoints still show up but the "Try
    It Out" buttons are hidden.

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -5,82 +5,138 @@ import axios, { AxiosResponse } from "axios";
 
 import Titlebar from "@components/Titlebar";
 
+type Credentials = {
+  address: string;
+  port: number;
+  username: string;
+  password: string;
+  protocol: string;
+};
+type SwaggerUIPlugged = Swagger & {
+  updateSpec: (specUpdates: any) => void;
+};
+
 const BASIC_AUTH = "BasicAuth";
 
 const Home = (): JSX.Element => {
-  const [credentials, SetCredentials] = useState<{
-    address: string;
-    port: number;
-    username: string;
-    password: string;
-    protocol: string;
-  }>();
+  const [swagger, SetSwagger] = useState<SwaggerUIPlugged>();
+  const [credentials, SetCredentials] = useState<Credentials>();
+  const [spec, SetSpec] = useState<any>();
 
+  // Get spec.
   useEffect(() => {
-    ipcRenderer.send("fe-ready");
-
-    ipcRenderer.on("credentialspass", (event, data) => {
-      SetCredentials(data);
-    });
+    axios
+      .get("https://www.mingweisamuel.com/lcu-schema/lcu/openapi.json")
+      .then((res: AxiosResponse<any>) => SetSpec(res.data));
   }, []);
 
-  const swaggerPromise = axios.get(
-    "https://www.mingweisamuel.com/lcu-schema/lcu/openapi.json"
-  );
-
+  // Setup credentials listener.
   useEffect(() => {
-    if (credentials == null) {
-      console.warn("Credentials are null.");
-      return;
-    }
-    swaggerPromise.then((res: AxiosResponse<any>) => {
-      const spec = res.data;
+    ipcRenderer.on("credentialspass", (_event, newCredentials) => {
+      console.log(`FE received credentials: ${JSON.stringify(newCredentials)}`);
+      SetCredentials(newCredentials);
+    });
+    ipcRenderer.send("fe-ready");
+  }, []);
 
-      spec.servers = [
-        {
-          url: `https://127.0.0.1:${credentials.port}`,
-          description: "default",
-        },
-      ];
-
-      spec.components = {
-        securitySchemes: {
-          [BASIC_AUTH]: {
-            type: "http",
-            scheme: "basic",
+  // Setup Swagger UI.
+  useEffect(() => {
+    const swaggerInst = Swagger({
+      dom_id: "#swagger",
+      spec: {
+        openapi: "3.0.0",
+        security: [
+          {
+            [BASIC_AUTH]: [],
+          },
+        ],
+        components: {
+          securitySchemes: {
+            [BASIC_AUTH]: {
+              type: "http",
+              scheme: "basic",
+            },
           },
         },
-      };
-      spec.security = [
-        {
-          [BASIC_AUTH]: [],
-        },
-      ];
+        servers: [],
+      },
+      operationsSorter: "alpha",
+      tagsSorter: "alpha",
+      docExpansion: "none",
+      defaultModelExpandDepth: 1,
+      displayRequestDuration: true,
+      filter: "",
+      deepLinking: false, // @ts-ignore
+      "request.curlOptions": ["--insecure"], // TODO: doesn't seem to show up.
+      plugins: [
+        (system) => ({
+          statePlugins: {
+            spec: {
+              wrapSelectors: {
+                allowTryItOutFor: () => () => {
+                  const jsonSpec = system.getState().toJSON().spec.json;
+                  return jsonSpec.servers.length > 0;
+                },
+              },
+            },
+          },
+          rootInjects: {
+            updateSpec: (specUpdates: any) => {
+              const jsonSpec = system.getState().toJSON().spec.json;
+              const newJsonSpec = { ...jsonSpec, ...specUpdates };
+              // Preserve securitySchemes.
+              newJsonSpec.components.securitySchemes =
+                jsonSpec.components.securitySchemes;
+              return system.specActions.updateJsonSpec(newJsonSpec);
+            },
+          },
+        }),
+      ],
+      onComplete: () => {
+        console.log("Swagger UI loading complete.");
+        SetSwagger(swaggerInst);
+      },
+    }) as SwaggerUIPlugged;
+  }, []);
 
-      try {
-        const swagger = Swagger({
-          dom_id: "#swagger",
-          spec,
-          operationsSorter: "alpha",
-          tagsSorter: "alpha",
-          docExpansion: "none",
-          defaultModelExpandDepth: 1,
-          displayRequestDuration: true,
-          filter: "",
-          deepLinking: false, // @ts-ignore
-          "request.curlOptions": ["--insecure"], // TODO: doesn't seem to show up.
-        });
-        swagger.preauthorizeBasic(
-          BASIC_AUTH,
-          credentials.username,
-          credentials.password
-        );
-      } catch (e) {
-        console.log("not ready to swagify");
-        console.log(e);
-      }
-    });
-  }, [credentials]);
+  useEffect(() => {
+    console.log(
+      `Updating FE: swagger ${swagger != null}, spec ${
+        spec != null
+      }, credentials ${JSON.stringify(credentials)}.`
+    );
+
+    // If Swagger UI not ready, nothing to update.
+    if (swagger == null) {
+      return;
+    }
+
+    // Update credentials/port from connector.
+    if (credentials != null) {
+      swagger.updateSpec({
+        servers: [
+          {
+            url: `https://127.0.0.1:${credentials.port}`,
+            description: "default",
+          },
+        ],
+      });
+      swagger.preauthorizeBasic(
+        BASIC_AUTH,
+        credentials.username,
+        credentials.password
+      );
+    } else {
+      swagger.updateSpec({
+        servers: [],
+      });
+    }
+
+    // Update OpenAPI spec lcu-schema.
+    if (spec != null) {
+      swagger.updateSpec(spec);
+    }
+  }, [swagger, credentials, spec]);
 
   return (
     <>
